d80121d652e7f2b508f57d24deefe597d8a44d8f
TwinHter <nhdnguyen05@gmail.com>
2024-07-22 22:45:40 +0700

    demo 2

--- Program.cs
+++ Program.cs
@@ -1,55 +1,97 @@
-﻿using System.Security.Cryptography;
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 
 namespace TicTacToe
 {
     class Program {
         public class GameState {
-            public int[,] current_table, previous_table;
+            public int[,] current_table;
+            public int[] previous_move;
             private int gameSize;
             private bool isExit;
             public int Move {get; set;}
-            private int ConvertCell(int x, int y) {
-                return x * gameSize + y + 1;
-            }
-            public GameState(int gameSize = 3) {
+            public GameState(int gameSize = 3) {    // Constructor
                 this.gameSize = gameSize;
                 current_table = new int[gameSize, gameSize];
-                previous_table = new int[gameSize, gameSize];
+                previous_move = new int[2]{-1, -1};
                 for(int i=0; i<gameSize; i++)
                 for(int j=0; j<gameSize; j++) {
-                    current_table[i, j] = previous_table[i, j] = ConvertCell(i, j);
+                    current_table[i, j] = ConvertCell(i, j);
                 }
                 Move = 0;
                 isExit = false;
             }
-            public void PrintGame() {
+            private int ConvertCell(int x, int y) { // Convert number of cell to pair of position
+                return x * gameSize + y + 1;
+            }
+            private void UpdatePrevious(int cell) { // Update for attribute previous move
+                if(previous_move[0] == -1) previous_move[0] = cell;
+                else {
+                    if(previous_move[1] == -1) previous_move[1] = cell;
+                    else {
+                        previous_move[1] = previous_move[0];
+                        previous_move[0] = cell;
+                    }
+                }
+            }
+            public void PrintGame() {   // Print current game table
                 for(int i=0; i<gameSize; i++) {
                     for(int j=0; j<gameSize; j++) {
                         string tmp = current_table[i, j].ToString();
                         if(current_table[i, j] == -2) tmp = "O";
-                        if(current_table[i, j] == -1) tmp = "X"; 
-                        Console.Write($"{tmp} |");
+                        if(current_table[i, j] == -1) tmp = "X";
+                        if(tmp.Length == 1) Console.Write($"{tmp} |");
+                        else Console.Write($"{tmp}|");
                     }
                     Console.WriteLine();
                 }
+                Console.WriteLine();
             }
-            public void HelpMessage() {
+            public void HelpMessage() { // Some keyword to control the game
                 System.Console.WriteLine("This is Help Message");
+                // System.Console.WriteLine("Type previous → Previous move\n
+                //                         Type restart → Restart game\n
+                //                         Type help → help\n
+                //                         Type exit → exit\n
+                //                         Type hint → cell can be play\n
+                //                         Type number -> choose the cell to write");
             }
             public void Restart() {
                 for(int i=0; i<gameSize; i++)
                 for(int j=0; j<gameSize; j++) {
-                    current_table[i, j] = previous_table[i, j] = ConvertCell(i, j);
+                    current_table[i, j] = ConvertCell(i, j);
                 }
+                Move = 0;
+                previous_move[0] = previous_move[1] = -1;
             }
             public void PreviousMove() {
-                current_table = previous_table;
+                foreach(int cell in previous_move) {
+                    if(cell > 0) {
+                        int x = (cell-1)/gameSize, y = (cell-1)%gameSize;
+                        current_table[x, y] = cell;
+                        Move -= 1;
+                    }
+                }
+                previous_move[0] = previous_move[1] = -1;
             }
-            public void HintMessage() {
-                System.Console.WriteLine("This is hint message");
+            public void HintMessage() { // Write valid cell which can be choosen.
+                List<int> valid_cell = new List<int>();
+                for(int i=0; i<gameSize; i++)
+                for(int j=0; j<gameSize; j++) {
+                    if(current_table[i, j] > 0) {
+                        valid_cell.Add(current_table[i, j]);
+                    }
+                }
+
+                Console.WriteLine($"There are {valid_cell.Count} valid cells.");
+                Console.Write("They are: ");
+                foreach(int cell in valid_cell) {
+                    Console.Write($"{cell} ");
+                }
+                Console.WriteLine();
             }
             public bool UserMove() {
-                Console.WriteLine("Type your move: ");
+                Console.Write("Type your move: ");
                 string inp = "";
                 while(string.IsNullOrEmpty(inp)) {
                     inp = Console.ReadLine();
@@ -57,14 +99,14 @@ namespace TicTacToe
                 
                 int num = 0;
                 if(int.TryParse(inp, out num)) {
-                    if(num > 0 && num < 9) {
+                    if(num > 0 && num <= gameSize*gameSize) {
                         int x = (num-1)/gameSize, y = (num-1)%gameSize;
                         if(current_table[x, y] < 0) {
                             System.Console.WriteLine("Cell have been choosen!");
                             return false;
                         }
                         else {
-                            previous_table = current_table;
+                            UpdatePrevious(num);
                             current_table[x, y] = -1;
                             return true;
                         }
@@ -96,22 +138,24 @@ namespace TicTacToe
                             Console.WriteLine("Invalid input");
                             break;
                     }
+                    if(!isExit) PrintGame();
                     return false;
                 }
             }
-            private void CPUMove() {
+            private void CPUMove() {    // Random algorithm for CPU Move
                 while(true) {
                     System.Random random = new System.Random(); 
-                    int move = random.Next(0, 9) + 1;
+                    int move = random.Next(0, gameSize*gameSize) + 1;
                     int x = (move-1)/gameSize, y = (move-1)%gameSize;
                     if(current_table[x, y] > 0) {
-                        previous_table = current_table;
+                        System.Console.WriteLine($"CPU choose cell {move}");
+                        UpdatePrevious(move);
                         current_table[x, y] = -2;
                         break;
                     }
                 }
             }
-            private void CPUAdvanceMove() {
+            private void CPUAdvanceMove() { // Intelligent algorithm for CPU Move
 
             }
             public int CheckResult() {
@@ -141,7 +185,7 @@ namespace TicTacToe
                             }
                         }
                         if(ok) {
-                            return (current_table[i, 0] == -1) ? 1 : 2;
+                            return (current_table[0, i] == -1) ? 1 : 2;
                         }
                     }
                 }
@@ -165,13 +209,14 @@ namespace TicTacToe
                     System.Console.WriteLine("Congratulation ! You are the winner");
                 }
                 if(state == 2) {
-                    System.Console.WriteLine("Try your best next time.");
+                    System.Console.WriteLine("CPU Win. Try your best next time.");
                 }
                 if(state == 3) {
                     System.Console.WriteLine("Draw !!! Another game?");
                 }
             }
-            public void GamePlay(int firstToMove = 1) {
+            public void GamePlay(int firstToMove = 1) { // Game turn base between CPU and User
+                PrintGame();
                 this.isExit = false;
                 int current_turn = firstToMove;
                 // current_turn = 1 -> UserMove, = 2 -> CPUMove
@@ -200,14 +245,21 @@ namespace TicTacToe
                         if(this.isExit) break;
                     }
                     else {
-                        CPUMove();
-                        Move += 1;
+                        if(Move < gameSize*gameSize) {
+                            CPUMove();
+                            current_turn = 3 - current_turn;
+                            Move += 1;
+                        }
                     }
+                    PrintGame();
                 }
                 System.Console.WriteLine("Thank you for your playing");
             }
         }  
         static void Main(string[] args) {
+            int gameSize = 3;
+            System.Console.Write("Type in game size of Tic Tac Toe game: ");
+            gameSize = Convert.ToInt32(Console.ReadLine());
             System.Console.WriteLine("Hello, this is TwinHter's Tic Tac Toe");
             System.Console.WriteLine("Type in 1 if you want to play first, otherwise type in integer larger than 1");
             int num = -1; 
@@ -223,7 +275,7 @@ namespace TicTacToe
                 }
             }
 
-            GameState new_game = new GameState();
+            GameState new_game = new GameState(gameSize);
             if(num == 1) new_game.GamePlay(1);
             else new_game.GamePlay(2);
         }
--- obj/Debug/net8.0/TicTacToe.AssemblyInfo.cs
+++ obj/Debug/net8.0/TicTacToe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2885d734adb9985c350a94763f4920c52471f1ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aaf152a1851e76020aa313a5faa4fcdb633619c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
